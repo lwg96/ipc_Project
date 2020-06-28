abcb86266d2ed0f39fa84d34891c6b9345e83dc0
lwg <642167809@qq.com>
2020-06-28 15:41:57 +0800

    修改了些东西

--- packages/hello-world/main.js
+++ packages/hello-world/main.js
@@ -1,3 +1,7 @@
+const {
+    loadFile
+} = require("./scene-Ctrl");
+
 module.exports = {
     load() {
         // 当 package 被正确加载的时候执行
@@ -14,12 +18,28 @@ module.exports = {
         },
 
         'find-all-spriteFrame'(event, data) {
-            Editor.assetdb.queryAssets('db://assets/**\/*', 'sprite-frame', function (err, results) {
-                Editor.log("find-all-spriteFrame:");
-                if (event.reply) {
-                    event.reply(results);
-                }
-            });
+            let moveAllDir = 'db://assets/movedAllDir';
+            let findSp = function () {
+                Editor.assetdb.queryAssets('db://assets/**\/*', 'sprite-frame', function (err, results) {
+                    Editor.log("find-all-spriteFrame:");
+                    if (event.reply) {
+                        event.reply(results);
+                    }
+                });
+            }
+            if (Editor.assetdb.exists(moveAllDir)) {
+                Editor.assetdb.delete(moveAllDir, function (err, results) {
+                    if (err) {
+                        Editor.log("删除目录失败:");
+                    } else {
+                        Editor.log("删除目录成功");
+                        findSp();
+                    }
+                })
+            } else {
+                findSp();
+            }
+
         },
 
         'hello-world:find-all-scene'(event) {
@@ -41,18 +61,56 @@ module.exports = {
         },
 
         'hello-world:move-All-UnuseSp'(event, data) {
+
+
+            let createDirLink = [];
+
+            let createDir = (dir) => {
+                Editor.log("队列:", createDirLink);
+                if (!Editor.assetdb.exists(dir) && createDirLink.indexOf(dir) == -1 ) {
+                    createDirLink.push(dir);
+                    Editor.assetdb.create(dir, "", function (err, results) {
+                        if (err) {
+                            Editor.log('创建目录失败:', dir);
+                        } else {
+                            createDirLink.shift();
+                            Editor.log("创建目录成功:", dir)
+                        }
+                    });
+                } else {
+                    Editor.log('目录已经存在了或者正在队列中:', dir);
+                }
+            }
+
+            let moveAllDir = 'db://assets/movedAllDir';
+            createDir(moveAllDir);
+
+
             for (let i = 0; i < data.length; i++) {
 
-                let str = Editor.assetdb.uuidToUrl(data[i]);
-                Editor.log("通过uuid获得的路径 = ", str);
-                let index = str.lastIndexOf("/");
-                Editor.log("最后一个/的位置 = ", index);
-                str = str.substr(0, index);
-                Editor.log("实际的路径 = ", str);
+                let uuidUrl = Editor.assetdb.uuidToUrl(data[i]);
+                Editor.log("通过uuid获得的路径 = ", uuidUrl);
 
-                let fileName = str.substr(str.lastIndexOf("/") + 1, str.length - 1);
+                let realPngUrl = uuidUrl.substring(0, uuidUrl.lastIndexOf("/"));
+                Editor.log("实际的路径 = ", realPngUrl);
+
+                let fileName = realPngUrl.substring(realPngUrl.lastIndexOf("/") + 1, realPngUrl.length);
                 Editor.log("fileName = ", fileName);
-                Editor.assetdb.move(str, 'db://assets/dynamy/' + fileName, function (err, results) {
+
+                let midUrl = realPngUrl.substring(12, realPngUrl.lastIndexOf("/")); //db://assets/
+                Editor.log("mid url = ", midUrl);
+
+                let midUrlArr = midUrl.split("/");
+                Editor.log("arr url = ", midUrlArr);
+
+                moveAllDir = 'db://assets/movedAllDir';
+
+                for (let j = 0, jLen = midUrlArr.length; j < jLen; j++) {
+                    moveAllDir = moveAllDir + "/" + midUrlArr[j];
+                    createDir(moveAllDir);
+                }
+
+                Editor.assetdb.move(realPngUrl, moveAllDir + '/' + fileName, function (err, results) {
                     if (err) {
                         Editor.log("有问题 = ", err);
                         return;
@@ -65,6 +123,12 @@ module.exports = {
                     // });
                 });
             }
+
+
+
+
+
+            // let a = Editor.assetdb.exists('db://assets/foo');
             // Editor.log("BBB = ", JSON.stringify(data));
         },
 
